0d3bdd06e7f49bd690397a3f8cc03cb0a6a2cde9
Fathismael93 <fathismael@gmail.com>
2025-03-04 06:34:31 -0800

    Created Cart model file

---
+++ backend/models/cart.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Cart || mongoose.model("Cart", cartSchema);
