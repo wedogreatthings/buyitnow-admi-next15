22d7f871eb4391af1a836a6f889d71e0ae872af0
Fathismael93 <fathismael@gmail.com>
2025-07-15 10:33:50 +0300

    Updated some mongoose models files, created @/utils/logger.js and installed winston

--- backend/models/paymentType.js
+++ backend/models/paymentType.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 const paymentTypeSchema = new mongoose.Schema({
   paymentName: {
     type: String,
-    required: [true, 'Please enter your platform name'],
+    required: true,
   },
   paymentNumber: {
     type: String,
-    required: [true, 'Please enter your platform number'],
+    required: true,
   },
 });
 
