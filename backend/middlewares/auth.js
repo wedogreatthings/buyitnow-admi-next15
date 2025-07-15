a3c9ba42af583bb8e3b42d84f06568adb60cd845
Fathismael93 <fathismael@gmail.com>
2025-07-15 11:59:06 +0300

    Updated model product.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,11 +4,16 @@ import auth from '@/pages/api/auth/[...nextauth]';
 import User from '../models/user';
 
 const isAuthenticatedUser = async (req, res, next) => {
+  console.log('Checking authentication for user:', req.user);
   const session = await getServerSession(req, res, auth);
+  console.log('Session retrieved:', session);
 
   if (!session) {
+    console.error('No session found. User is not authenticated.');
     return new ErrorHandler('Login first to access this route', 401);
   }
+
+  console.log('Session user:', session.user);
 
   req.user = session.user;
 
@@ -17,13 +22,21 @@ const isAuthenticatedUser = async (req, res, next) => {
 
 const authorizeRoles = (...roles) => {
   return async (req, res, next) => {
+    console.log('Authorizing user with roles:', req.user.role);
     const user = await User.findOne({ email: req.user.email }).select('role');
 
     if (!roles.includes(user.role)) {
+      console.error(
+        `User role (${user.role}) is not authorized. Required roles: ${roles.join(
+          ', ',
+        )}`,
+      );
       return new ErrorHandler(
         `Role (${user.role}) is not allowed to access this resource.`,
       );
     }
+
+    console.log('User is authorized:', user.role);
 
     next();
   };
