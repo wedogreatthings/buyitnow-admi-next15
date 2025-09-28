170bd25fb7475cafcc6c5dc6903558b492b78a88
Fathismael93 <fathismael@gmail.com>
2025-09-28 12:32:46 +0300

    Updated some files

--- app/api/cloudinary/sign/route.js
+++ app/api/cloudinary/sign/route.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { NextResponse } from 'next/server';
 
-// Configuration Cloudinary
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
